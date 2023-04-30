a6980e26a423f84d9999d3eaddb888096bba886c
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2023-04-30 14:39:28 +0530

    accepting JSON requests now

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,10 +6,38 @@ import (
 	"time"
 
 	"github.com/mrityunjaygr8/greenlight/internal/data"
+	"github.com/mrityunjaygr8/greenlight/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a movie")
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Genres:  input.Genres,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+	}
+
+	v := validator.New()
+
+	data.ValidateMovie(v, movie)
+
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	fmt.Fprintf(w, "%+v\n", input)
 }
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
