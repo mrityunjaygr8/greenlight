4b22df3e11bb41f0c2080a9bf11e4a411b9e2016
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2023-04-30 18:03:43 +0530

    using task instead of make now

---
+++ default.nix
@@ -0,0 +1,10 @@
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  # nativeBuildInputs is usually what you want -- tools you need to run
+  nativeBuildInputs = [
+    # pkgs.go
+    pkgs.fish
+    pkgs.go-migrate
+    pkgs.go-task
+  ];
+}
